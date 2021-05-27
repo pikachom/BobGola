80e27d9317d3c808c7abf32bbd8a0ce30bc71b78
pikachom <pikachom@gmail.com>
2021-05-28 03:58:37 +0900

    주석 업데이트

--- src/main/java/crawler/CrawlerApp.java
+++ src/main/java/crawler/CrawlerApp.java
@@ -3,9 +3,8 @@ package crawler;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
 
 public class CrawlerApp {
     ConfigurationBuilder cb;
@@ -18,12 +17,33 @@ public class CrawlerApp {
                 .setOAuthConsumerSecret("QglxPmMEqTnOIZTbhBGoLJ874enXY7ky4dq9Rydk5p0rWQGbGc")
                 .setOAuthAccessToken("245304374-jc9nEywA1ftVVbo8j83mKcQPfjFVyxaOJxpaWTZG")
                 .setOAuthAccessTokenSecret("MeIVBtor9pqf0Bew24XL46LlK7LqDPe9dfCmqwCbl5y6X");
+//        String resource = "main/resources/twitter.properties";
+//        Properties properties = new Properties();
+//        try{
+//            InputStream reader = getClass().getClassLoader().getResourceAsStream(resource);
+////            Reader reader = Resources.getResourceAsReader(resource);
+//            properties.load(reader);
+//            String consumerKey = properties.getProperty("consumerkey");
+//            String consumerSecret = properties.getProperty("consumersecret");
+//            String accessToken = properties.getProperty("accesstoken");
+//            String accesstokenSecret = properties.getProperty("accesstokensecret");
+//            cb.setDebugEnabled(true)
+//                    .setOAuthConsumerKey(consumerKey)
+//                    .setOAuthConsumerSecret(consumerSecret)
+//                    .setOAuthAccessToken(accessToken)
+//                    .setOAuthAccessTokenSecret(accesstokenSecret);
+//
+//        } catch (IOException e){
+//            System.out.println("config file not found");
+////            e.printStackTrace();
+//        }
+
         this.tf = new TwitterFactory(cb.build());
         this.twitter = tf.getInstance();
     }
     public void doCrawl() throws IOException, TwitterException {
         BufferedWriter bw = new BufferedWriter(new FileWriter("tweets.txt"));
-        Query query = new Query("menu");
+        Query query = new Query("메뉴");
         QueryResult result = twitter.search(query);
         for(Status status : result.getTweets()){
             bw.write("@" + status.getUser().getScreenName() + ":"
